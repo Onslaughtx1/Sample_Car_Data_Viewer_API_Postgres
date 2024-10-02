49d3983951c1905b25b1e8640f9efd679a39b4e4
Michael Ferguson <mikeygsxr1k@gmail.com>
2024-10-02 16:46:33 +0200

    Add project files.

---
+++ Sample_Car_Data_Viewer_API_Postgres.Server/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Sample_Car_Data_Viewer_API_Postgres.Server
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
