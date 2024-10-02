d5f084ef7eeead62d72933f5573dd8f83d6d3c46
Michael Ferguson <mikeygsxr1k@gmail.com>
2024-10-02 18:13:22 +0200

    POCO Created and API Returns List of Cars

---
+++ Sample_Car_Data_Viewer_API_Postgres.Server/Controllers/CarsDataController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sample_Car_Data_Viewer_API_Postgres.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarsDataController : ControllerBase
+    {
+        private readonly ILogger<CarsDataController> _logger;
+
+        public CarsDataController(ILogger<CarsDataController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetCarList")]
+        public ActionResult<IEnumerable<CarsData>> GetCars()
+        {
+            var dbOps = new DatabaseOperations();
+            var cars = dbOps.PgsqlConnection_GetCarsData();
+            //var jsonString = JsonSerializer.Serialize(cars);
+            //retirn jsonString;
+            return cars;
+        }
+    }
+}
